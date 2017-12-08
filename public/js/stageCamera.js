e6855068f7d2e53eb89b74e52c1b2e73086553ef
Kinslow <simon.kinslow@aa7778.weu.grey.global>
2017-12-08 11:24:21 +0000

    Center section working
    Offset outline replaces border because it was adding extra height on the camera div

--- public/js/stageCamera.js
+++ public/js/stageCamera.js
@@ -31,12 +31,14 @@ class Camera
 	{
 		var vf = {};
 
-		vf.cx = -target.x;
-		vf.cy = -target.y;
+		vf.cx = -((target.x));
+		vf.cy = -((target.y));
+		vf.cx_f = vf.cx + ((this.w * 0.5) - (target.w * 0.5));
+		vf.cy_f = vf.cy + ((this.h * 0.5) - (target.h * 0.5));
 
 		trace(vf);
 
-		this.viewerShift(vf.cx, vf.cy);
+		this.viewerShift(vf.cx_f, vf.cy_f);
 	}
 
 	viewerShift(x, y)
