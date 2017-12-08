86950db2b0017b563de7fbb1be6bc169bc8b5c4a
Kinslow <simon.kinslow@aa7778.weu.grey.global>
2017-12-08 09:58:49 +0000

    Basics shift and update values added

--- public/js/stageCamera.js
+++ public/js/stageCamera.js
@@ -12,6 +12,9 @@ class Camera
 	{
 		this.w = this.main.offsetWidth;
 		this.h = this.main.offsetHeight;
+		
+		this.x = 0;
+		this.y = 0;
 	}
 
 	connectViewer(div)
@@ -19,6 +22,46 @@ class Camera
 		this.viewer = div;
 		this.viewW = this.viewer.offsetWidth;
 		this.viewH = this.viewer.offsetHeight;
+		
+		this.viewer.x = 0;
+		this.viewer.y = 0;
+	}
+
+	viewerShift(x, y)
+	{
+		this.viewer.x = x;
+		this.viewer.y = y;
+		
+		if(this.viewer.x != this.x || this.viewer.y != this.y)
+		{
+			this.viewerTransition();
+		}
+
+		trace(this.viewerTransition);
+	}
+
+	viewerTransition()
+	{
+		this.viewer.addEventListener("transitionend", this.viewerTransitionEvent, false);
+
+		this.viewer.setAttribute("style", "transform: translate(" + this.viewer.x + "px, " + this.viewer.y + "px);");
+	}
+
+	// OUT OF SCOPE
+	viewerTransitionEvent(event)
+	{
+		event.target.removeEventListener("transitionend", this.viewerTransitionEvent, false);
+		
+		trace(event);
+
+		// LINK BACK VALUES
+		camera_newFocus();
+	}
+
+	viewerUpdateValues()
+	{
+		this.x = this.viewer.x;
+		this.y = this.viewer.y;	
 	}
 }
 
@@ -40,17 +83,36 @@ function project_setup()
 	displayList.camera = document.querySelector(".camera");
 	displayList.viewer = document.querySelector(".viewer");
 
+	trace(displayList.viewer.style);
+
 	camera_init();
 }
 
 function camera_init()
 {
-	var CAM = new Camera(displayList.camera);
+	CAM = new Camera(displayList.camera);
 
 	CAM.updateResizeCamera();
 	CAM.connectViewer(displayList.viewer);
 
 	trace(CAM);
 
+	var delay = setTimeout(camera_test1, 1 * 1000);
 }
+
+function camera_test1()
+{
+	CAM.viewerShift(-500, -500);
+}
+
+function camera_newFocus()
+{
+	CAM.viewerUpdateValues();
+}
+
+
+
+
+
+
 
